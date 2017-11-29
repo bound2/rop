b20e6f3b255b1a092a202acdbec83eed47c8bc16
bound2 <9380102+bound2@users.noreply.github.com>
2017-11-29 17:14:39 +0200

    remove dependency

--- src/test/java/com/snyberichapp/tests/RopTestngTest.java
+++ src/test/java/com/snyberichapp/tests/RopTestngTest.java
@@ -67,7 +67,7 @@ public class RopTestngTest {
                 .newLine()
                 .assertEquals("[2].name", "jansen_array")
                 .assertEquals("[2].kidCount", "8")
-                .assertEquals("[2].married", "null")
+                .assertEquals("[2].married", "null") // TODO assertEmpty, assertNotExist
                 .assertEquals("[2].born", DateTimeFormatter.ISO_INSTANT.format(instant))
                 .assertEquals("[2].died", DateTimeFormatter.ISO_INSTANT.format(instant))
         ;
