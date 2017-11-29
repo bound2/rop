5eddb9633f0a001d24e3d64a608c1e1acd0619f7
bound2 <9380102+bound2@users.noreply.github.com>
2017-11-29 16:49:37 +0200

    add array assertion functionality

--- src/test/java/com/snyberichapp/common/Rop.java
+++ src/test/java/com/snyberichapp/common/Rop.java
@@ -4,14 +4,16 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.exc.MismatchedInputException;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.Map;
-import java.util.TimeZone;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public final class Rop {
 
@@ -19,24 +21,76 @@ public final class Rop {
     private static final ObjectMapper OM = initObjectMapper();
     private static final TypeReference<Map<String, Object>> TYPE_REFERENCE = new TypeReference<Map<String, Object>>() {
     };
+    private static final TypeReference<List<Map<String, Object>>> LIST_TYPE_REFERENCE = new TypeReference<List<Map<String, Object>>>() {
+    };
+    private static final Pattern ARRAY_ELEMENT_PATTERN = Pattern.compile("\\[\\d+\\]");
     private static TestConfiguration testConfiguration;
 
-    private final Map<String, Object> values;
+    private Map<String, Object> values;
+    private List<Map<String, Object>> arrayValues;
 
     private Rop(Object object) throws IOException {
         String json = OM.writeValueAsString(object);
-        this.values = OM.readValue(json, TYPE_REFERENCE);
+        try {
+            this.values = OM.readValue(json, TYPE_REFERENCE);
+        } catch (MismatchedInputException e) {
+            this.arrayValues = OM.readValue(json, LIST_TYPE_REFERENCE);
+        }
+    }
+
+    public Rop newLine() {
+        return this;
+    }
+
+    public Rop assertArraySize(int expectedSize) {
+        ResultComparison resultComparison = new ResultComparison(String.valueOf(arrayValues.size()), String.valueOf(expectedSize));
+        testConfiguration.equalsConsumer().accept(resultComparison);
+        return this;
     }
 
     public Rop assertEquals(String key, String expectedValue) {
-        // TODO handle map etc from actual value
-        final String actualValue;
-        final Object value = values.get(key);
-        actualValue = value.toString();
-
+        String actualValue = findValue(key);
         ResultComparison resultComparison = new ResultComparison(actualValue, expectedValue);
         testConfiguration.equalsConsumer().accept(resultComparison);
         return this;
+    }
+
+    private String findValue(String key) {
+        LinkedList<String> tokens = new LinkedList<>(Arrays.asList(key.split("\\.")));
+        final String value;
+        if (tokens.size() <= 1) {
+            value = values.get(key).toString();
+        } else {
+            // something.different.arrays[0].value
+            // [0].something.different.arrays[0].value
+            String firstToken = tokens.removeFirst();
+            Matcher firstMatcher = ARRAY_ELEMENT_PATTERN.matcher(firstToken);
+            Map<String, Object> element;
+            if (firstMatcher.find()) {
+                element = arrayValues.get(getArrayElement(firstMatcher));
+            } else {
+                element = (Map<String, Object>) values.get(firstToken);
+            }
+
+            String lastToken = tokens.removeLast();
+            for (String token : tokens) {
+                Matcher matcher = ARRAY_ELEMENT_PATTERN.matcher(token);
+                if (matcher.find()) {
+                    List<Map<String, Object>> array = (List<Map<String, Object>>) element.get(token);
+                    element = array.get(getArrayElement(matcher));
+                } else {
+                    element = (Map<String, Object>) element.get(token);
+                }
+            }
+            value = element.get(lastToken).toString();
+        }
+        return value;
+    }
+
+    private Integer getArrayElement(Matcher matcher) {
+        return Integer.valueOf(matcher.group()
+                .replace("[", "")
+                .replace("]", ""));
     }
 
     // ========================= HELPER METHODS ========================= //
--- src/test/java/com/snyberichapp/tests/RopTestngTest.java
+++ src/test/java/com/snyberichapp/tests/RopTestngTest.java
@@ -8,7 +8,9 @@ import org.testng.annotations.Test;
 
 import java.time.Instant;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 public class RopTestngTest {
 
@@ -32,7 +34,42 @@ public class RopTestngTest {
 
     @Test
     public void arrayObjectTest() throws Exception {
+        Calendar calendar = Calendar.getInstance();
+        Instant instant = Instant.now();
 
+        List<RegularTestObject> objects = new ArrayList<>();
+        {
+            RegularTestObject testObject = new RegularTestObject("sven_array", 14, Boolean.TRUE, calendar, instant);
+            objects.add(testObject);
+        }
+        {
+            RegularTestObject testObject = new RegularTestObject("frodo_array", 2, Boolean.FALSE, calendar, instant);
+            objects.add(testObject);
+        }
+        {
+            RegularTestObject testObject = new RegularTestObject("jansen_array", 8, null, calendar, instant);
+            objects.add(testObject);
+        }
+
+        Rop.of(objects).assertArraySize(3)
+                .assertEquals("[0].name", "sven_array")
+                .assertEquals("[0].kidCount", "14")
+                .assertEquals("[0].married", "true")
+                .assertEquals("[0].born", DateTimeFormatter.ISO_INSTANT.format(instant))
+                .assertEquals("[0].died", DateTimeFormatter.ISO_INSTANT.format(instant))
+                .newLine()
+                .assertEquals("[1].name", "frodo_array")
+                .assertEquals("[1].kidCount", "2")
+                .assertEquals("[1].married", "false")
+                .assertEquals("[1].born", DateTimeFormatter.ISO_INSTANT.format(instant))
+                .assertEquals("[1].died", DateTimeFormatter.ISO_INSTANT.format(instant))
+                .newLine()
+                .assertEquals("[2].name", "jansen_array")
+                .assertEquals("[2].kidCount", "8")
+                .assertEquals("[2].married", "null")
+                .assertEquals("[2].born", DateTimeFormatter.ISO_INSTANT.format(instant))
+                .assertEquals("[2].died", DateTimeFormatter.ISO_INSTANT.format(instant))
+        ;
     }
 
     @Test
